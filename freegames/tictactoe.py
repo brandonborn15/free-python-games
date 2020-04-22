e75e1e5cc09ac6cd08e495d2cf8eeca7e56a9862
Thomas Walsh <lwalsh7@luc.edu>
2020-04-22 02:02:47 -0500

    Update tictactoe.py

--- freegames/tictactoe.py
+++ freegames/tictactoe.py
@@ -8,7 +8,7 @@ Exercises
 4. How could you create a computer player?
 
 """
-
+import random
 from turtle import *
 import time
 from freegames import line
@@ -23,6 +23,7 @@ box6 = ''
 box7 = ''
 box8 = ''
 box9 = ''
+rando = 32
 
 def grid():
     "Draw tic-tac-toe grid."
@@ -54,15 +55,17 @@ def checkwin():
     for scenario in wins:
         if scenario[0] == scenario[1] == scenario[2] and scenario[0] != '':
             print("CONGRATULATIONS, " + scenario[0] + ' wins!')
+            clearBoard()
             main()
 
 def floor(value):
     "Round value down to grid with square size 133."
+    print(((value + 200) // 133) * 133 - 200)
     return ((value + 200) // 133) * 133 - 200
 
 def aiturn():
     "AI's turn"
-    global box1, box2, box3, box4, box5, box6, box7, box8, box9
+    global box1, box2, box3, box4, box5, box6, box7, box8, box9, rando
     wins = [[box1, box2, box3], [box4, box5, box6], [box7, box8, box9], [box1, box4, box7], [box2, box5, box8], [box3, box6, box9], [box1, box5, box9], [box3, box5, box7]]
     prospects = []
     for scenario in wins:
@@ -74,27 +77,69 @@ def aiturn():
                 xcount += 1
             elif box == '':
                 spacecount += 1
-            else:
+            elif box == 'o':
                 ocount += 1
         prospects.append([scenario, xcount, spacecount, ocount])
     for prospect in prospects:
         if prospect[3] == 2 and prospect[2] == 1:
             for box in prospect[0]:
                 if box == '':
-                    box = 'o'
-                    #CODE TO DRAW THE O
-                    checkwin()
-                    update()
+                    box = 'tbd'
+                    print("CHECKMATE")
+                    makeChoice()
     for prospect in prospects:
         if prospect[1] == 2 and prospect[2] == 1:
             for box in prospect[0]:
                 if box == '':
-                    box = 'o'
-                    #CODE TO DRAW THE O
-                    checkwin()
-                    update()
-    #CODE TO PICK A RANDOM NUMBER 1-9 and check to see if it has been chosen already
-    #CODE TO DRAW THE O
+                    box = 'tbd'
+                    print("BLOCKED")
+                    makeChoice()
+    boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+    valid = False
+    while valid == False:
+        tempnum = random.randrange(0, 8)
+        if boxes[tempnum] == '':
+            print(box1 + '\n' + box2 + '\n' + box3 + '\n' + box4 + '\n' + box5 + '\n' + box6 + '\n' + box7 + '\n' + box8 + '\n' + box9)
+            boxes[tempnum] = 'tbd'
+            print(box1 + '\n' + box2 + '\n' + box3 + '\n' + box4 + '\n' + box5 + '\n' + box6 + '\n' + box7 + '\n' + box8 + '\n' + box9)
+            rando = tempnum
+            print(tempnum)
+            valid = True
+            print("idk")
+            makeChoice()
+
+def makeChoice():
+    global box1, box2, box3, box4, box5, box6, box7, box8, box9, rando
+    boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+    theChosenOne = 32
+    for box in boxes:
+        if box == 'tbd':
+            theChosenOne = boxes.index(box)
+            box = 'o'
+    if theChosenOne == 32:
+        theChosenOne = rando
+    if theChosenOne == 0:
+        drawo(-200.0, 66.0)
+        print("1")
+    elif theChosenOne == 1:
+        drawo(-67.0, 66.0)
+    elif theChosenOne == 2:
+        drawo(66.0, 66.0)
+    elif theChosenOne == 3:
+        drawo(-200.0, -67.0)
+    elif theChosenOne == 4:
+        drawo(-67.0, -67.0)
+    elif theChosenOne == 5:
+        drawo(66.0, -67.0)
+    elif theChosenOne == 6:
+        drawo(-200.0, -200.0)
+    elif theChosenOne == 7:
+        drawo(-67.0, -200.0)
+    elif theChosenOne == 8:
+        drawo(66.0, -200.0)
+    else:
+        print("ERROR")
+    checkwin()
     update()
 
 def tap(x, y):
@@ -151,4 +196,17 @@ def main():
     update()
     onscreenclick(tap)
     done()
+
+def clearBoard():
+    global box1, box2, box3, box4, box5, box6, box7, box8, box9
+    box1 = ''
+    box2 = ''
+    box3 = ''
+    box4 = ''
+    box5 = ''
+    box6 = ''
+    box7 = ''
+    box8 = ''
+    box9 = ''
+
 main()
